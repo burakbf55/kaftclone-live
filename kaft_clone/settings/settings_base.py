f347c606dacf2ab9e38093ae2b98008a4d2653de
burakbf55 <burak0009000@gmail.com>
2021-09-25 23:00:53 +0300

    Whitenoise middleware, static_root

--- kaft_clone/settings/settings_base.py
+++ kaft_clone/settings/settings_base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'kaft_clone.middleware.session_check_middleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR,'../staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'../static'),
